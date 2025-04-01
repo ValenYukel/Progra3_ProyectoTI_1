13454225051ebb14a17aa5cc8d1b0391e6bb30e5
ValenYukel <vyukelson@udesa.edu.ar>
2025-04-01 14:08:20 -0300

    cambios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import {Route, Switch } from 'react-router-dom'
 import Home from "./screens/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
--- my-app/src/screens/Home/Home.js
+++ my-app/src/screens/Home/Home.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Header from "../../components/Header/Header";
 
 function Home(){
     return(
-    
-      <h2>This is Screen HOME</h2>
-    
+      <h2>THIS IS SCREEN HOME</h2>    
     )
 }
 

66835d704fee0c08cb69c836517cab879af563f8
maximo freijomil <freijomilmaximo@gmail.com>
2025-04-09 19:33:05 -0300

    conexion con moviedb,datos de peliculas de cartelera traidos al home, mostrar descripcion e imagen

--- my-app/src/Components/SeccionCartel/SeccionCartel.js
+++ my-app/src/Components/SeccionCartel/SeccionCartel.js
@@ -1,32 +1,94 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Cartel from './Cartel'
 import './styles.css'
-export default function SeccionCarteles() {
-    const Carteles = [
-        {
-            name: 'Cartel 1',
-        },
-        {
-            name: 'Cartel 2',
-        },
-        {
-            name: 'Cartel 3',
-        },
-        {
-            name: 'Cartel 4',
-        },
-        {
-            name: 'Cartel 5',
-        },
-        {
-            name: 'Cartel 6',
-        },
-    ]
-  return (
+
+export default class SeccionCartel extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            dataPelicula: props.data,
+            favorito: false,
+            mostrarContenido: false
+        }
+    }
+
+  componentDidMount(){
+      let storage= localStorage.getItem('favorito')
+      if(storage !== null){
+        let storageParseado = JSON.parse(storage)
+        let estaMiId = storageParseado.includes(this.state.dataPelicula.id)
+
+        if(estaMiId){
+          this.setState({favorito: true})
+        }
+      }
+    }
+    ocultar() {
+        this.setState({
+            mostrarContenido: !this.state.mostrarContenido
+        })
+    }
+    agregarAFavorito(id){
+        let storage = localStorage.getItem('favorito')
+        if(storage !== null){
+          let arrParseado = JSON.parse(storage)
+          arrParseado.push(id)
+          let arrStringificado = JSON.stringify(arrParseado)
+          localStorage.setItem('favorito', arrStringificado)
+        } else {
+          let primerID = [id]
+          let arrStringificado = JSON.stringify(primerID)
+          localStorage.setItem('favorito', arrStringificado)
+        }
+  
+        this.setState({
+          favorito: true
+        })
+      }
+  
+      sacarDeFavorito(id){
+        const storage = localStorage.getItem('Favorito')
+        const storageParseado = JSON.parse(storage)
+        const filtrarStorage = storageParseado.filter((elm) => elm !== id )
+        const storageStringificado = JSON.stringify(filtrarStorage)
+        localStorage.setItem('Favorito', storageStringificado)
+  
+        this.setState({
+          favorito: false
+        })
+      }
+  
+    render() {
+      return (
+        <div className='general-data'>
+          <h1>{this.state.dataPelicula.original_title}</h1>
+          <img src={'https://image.tmdb.org/t/p/w300/' + this.state.dataPelicula.poster_path} />
+          
+          {
+            this.state.favorito ?
+            <button onClick={()=> this.sacarDeFavorito(this.state.dataPelicula.id) }>Sacar del Favorito</button>
+            :
+            <button onClick={() => this.agregarAFavorito(this.state.dataPelicula.id)}>Agregar al Favorito</button>
+          }
+          <div className={this.state.mostrarContenido ? '' : 'hide'}>
+                    <p>
+                        {this.state.dataPelicula.overview}
+                    </p>
+                   
+                </div>
+                <button onClick={() => this.ocultar()} >
+                    Ver descripcion
+                </button>
+         </div>
+      )
+    }
+  }
+
+  /*return (
     <section className='general-data'>
         {
             Carteles.map((elm, idx)=> <Cartel key={`${idx}-${elm.name}`} data={elm} />)
         }
     </section>
-  )
-}
+  )*/
+
--- my-app/src/screens/Home/Home.js
+++ my-app/src/screens/Home/Home.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import react, {Component} from "react";
 import SeccionPopulares from '../../Components/SeccionPopulares/SeccionPopulares';
 import SeccionCarteles from '../../Components/SeccionCartel/SeccionCartel';
-function Home(){
+
+/*function Home(){
     return(
     <React.Fragment>   
        <h1>Home</h1>
@@ -14,6 +15,57 @@ function Home(){
 
     </React.Fragment>   
     )
-};
+};*/
+
+const api_key = "14c41ab32cccfc97ee8d878a2ca4b3ac"
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            Home: [],
+            backupHome: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=' + api_key)
+        .then((response) => response.json())
+        .then(( data ) => this.setState({
+          
+          Home:data.results, 
+          backupHome: data.results
+          //console.log(data))
+        })) 
+        .catch((error) => console.log(error) );
+    }
+
+    filtrarHome(busquedaUsuario){
+        const HomeFiltrados = this.state.backupHome.filter(
+            (elm) => elm.name.toLowerCase().includes(busquedaUsuario.toLowerCase())
+        )
+        this.setState({Home: HomeFiltrados})
+    }
+
+    render(){
+        return(
+            <>
+            <h1>Peliculas en cartelera</h1>
+
+            {
+                this.state.Home.length === 0 ?
+                <h1>Cargando Home de Cartelera</h1>
+                :
+                this.state.Home.map((elm, idx) => <SeccionCarteles data={elm} key={idx + elm.name} /> 
+              
+                )
+
+            }
+            </>
+        )
+    }
+}
+
+
 
 export default Home;

0d67f3c99a981709614e81f92943559f43104755
BenjaOrchansky <borchansky@udesa.edu.ar>
2025-04-13 23:30:11 -0600

    detallito en el NotFound

--- my-app/src/screens/NotFound/NotFound.js
+++ my-app/src/screens/NotFound/NotFound.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './styles.css'
 
 
 function NotFound() {
@@ -7,7 +8,8 @@ function NotFound() {
         <div className="not-found">
             <h1>Error 404 - Not Found</h1>
             <p>Esta ruta no existe, estas perdido Vaquero</p>
-            <Link to="/">Volver al Inicio</Link>
+            <img src='/img/tumbleweed.gif' alt='Cargando...' />
+            <Link to="/" className="boton"><button> Volver ;)</button></Link>
         </div>
     );
 }
